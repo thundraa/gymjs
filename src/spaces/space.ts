5f460eb3bc3828a3ffe4ec7b5031a8178aed9d8c
unexploredtest <unexploredtest@tutanota.com>
2025-09-13 21:30:17 +0330

    Add abstract class Space

---
+++ src/spaces/space.ts
@@ -0,0 +1,16 @@
+import * as tf from '@tensorflow/tfjs';
+
+
+abstract class Space {
+    public shape: number[];
+    public dtype: tf.DataType;
+    
+    constructor(shape: number[], dtype: tf.DataType) {
+        this.shape = shape;
+        this.dtype = dtype;
+    }
+    
+    abstract sample(): tf.Tensor | number;
+}
+
+export { Space }
