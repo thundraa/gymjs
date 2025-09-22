5b857a61c640e72467823585018d083917406100
unexploredtest <unexploredtest@tutanota.com>
2025-09-22 21:26:39 +0330

    Add base class Env

---
+++ src/core.ts
@@ -0,0 +1,28 @@
+import * as tf from '@tensorflow/tfjs';
+
+import { Discrete } from './spaces/discrete';
+import { Box } from './spaces/box';
+
+type ActSpace = Discrete;
+type ObsSpace = Box;
+
+type InfoType = Record<string, number | string | number[] | string[] | tf.Tensor>;
+
+abstract class Env {
+    protected renderMode: string;
+    public actionSpace: ActSpace;
+    public observationSpace: ObsSpace;
+
+    constructor(actionSpace: ActSpace, observationSpace: ObsSpace, renderMode: string) {
+        this.actionSpace = actionSpace;
+        this.observationSpace = observationSpace;
+        this.renderMode = renderMode;
+    }
+
+    abstract reset(): [tf.Tensor, InfoType | null];
+    abstract step(action: tf.Tensor | number): Promise<[tf.Tensor, number, boolean, boolean, InfoType | null]>; // Action is number for now
+    abstract render(): Promise<void | tf.Tensor>;
+    abstract close(): void;
+}
+
+export { Env }
