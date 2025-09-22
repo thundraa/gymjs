6e535be84255d08b1e3e669dca854749cffee811
unexploredtest <53617231+unexploredtest@users.noreply.github.com>
2025-09-22 14:41:39 +0000

    Add Space class and the spaces discrete and box (#9)
    * Add abstract class Space
    
    * Add space discrete
    
    * Add space box
    
    * Fix formatting issues

---
+++ src/spaces/discrete.ts
@@ -0,0 +1,26 @@
+import * as tf from '@tensorflow/tfjs';
+import { Space } from './space';
+
+class Discrete extends Space {
+    public n: number;
+    public start: number;
+    
+    constructor(n: number, start: number = 0) {
+        super([], "int32")
+        this.n = n;
+        this.start = start;
+    }
+
+    sample(): number {
+        // Might be a bit too elaborate to use TF but in case we use seeds this is the way
+        let randomNumTensor = tf.randomUniformInt([1], this.start, this.start + this.n);
+        let [randomNumber] = randomNumTensor.dataSync();
+        if(randomNumber === undefined) {
+            throw new Error("Random number can't be undefined!");
+        }
+
+        return randomNumber;
+    }
+}
+
+export { Discrete }
